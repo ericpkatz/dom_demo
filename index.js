c7db13d8c30775d8252b035188e992c22d6dd746
ericpkatz <ericpkatz@gmail.com>
2023-11-28 12:09:25 -0500

    show numbers

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const h1 = window.document.querySelector('h1');
+const numbersList = document.querySelector('#numbersList');
+const oddsList = document.querySelector('#oddsList');
 
-window.setTimeout(()=>{
-  h1.innerHTML = 'Hello World';
-}, 2000);
+const numbers = [1, 2, 3, 4, 55, 66, 72];
+
+const html = numbers.map(function(num){
+  return `<li>${num}</li>`;
+}).join('');
+
+numbersList.innerHTML = html;
+
+const odds = numbers.filter(function(num){
+  return num % 2 === 1;
+});
+
+const oddsHtml = odds.map(function(num){
+  return `<li>${num}</li>`;
+}).join('');
+
+
+oddsList.innerHTML = oddsHtml;
+
