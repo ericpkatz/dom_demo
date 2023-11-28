2832a4d491250bd24cda6d70436e039af8c8c332
ericpkatz <ericpkatz@gmail.com>
2023-11-28 12:29:30 -0500

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const numbersList = document.querySelector('#numbersList');
 const oddsList = document.querySelector('#oddsList');
+const evensList = document.querySelector('#evensList');
+
 
 const numbers = [1, 2, 3, 4, 55, 66, 72];
 
@@ -19,4 +21,14 @@ const oddsHtml = odds.map(function(num){
 
 
 oddsList.innerHTML = oddsHtml;
+
+const evens = numbers.filter(function(num){
+  return num % 2 === 0;
+});
+
+const evensHtml = evens.map(function(num){
+  return `<li>${num}</li>`;
+}).join('');
+
+evensList.innerHTML = evensHtml;
 
